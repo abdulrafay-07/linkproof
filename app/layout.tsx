3b0c6d3e21169fab6a04ceaaf10d8964d72663b7
Abdul Rafay <152396110+abdulrafay-07@users.noreply.github.com>
2024-08-31 22:48:25 +0500

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 import { Navbar } from '@/components/navbar';
 import { Footer } from '@/components/footer';
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from '@vercel/analytics/react';
 
 const font = Poppins({
   subsets: ['latin'],
@@ -32,6 +33,7 @@ export default function RootLayout({
           </main>
           <Footer />
           <Toaster />
+          <Analytics />
         </div>
       </body>
     </html>
